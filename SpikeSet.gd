c2aab8ca4340847f9e97a845a0a51461fa48c115
alexandre <thiry.alexandre@gmail.com>
2020-02-05 23:13:43 +0100

    tool mode on spikes for preview in world + bugfixes

--- SpikeSet.gd
+++ SpikeSet.gd
@@ -1,13 +1,54 @@
+tool
 extends Node2D
 
 export (PackedScene) var spike_scene
-export (int) var width_left = 0
-export (int) var width_right = 0
+export (int) var width_left = 0 setget _set_width_left
+export (int) var width_right = 0 setget _set_width_right
 
 func _ready():
-	$AnimatedSprite.visible = false
-	var spike
-	spike = spike_scene.instance()
+	if not Engine.editor_hint:
+		$AnimatedSprite.visible = false
+		var spike
+		spike = spike_scene.instance()
+		add_child(spike)
+		spike.position = Vector2()
+		for i in range(width_left):
+			spike = spike_scene.instance()
+			add_child(spike)
+			spike.position.x = (i + 1) * -32
+			spike.position.y = 0
+		for i in range(width_right):
+			spike = spike_scene.instance()
+			add_child(spike)
+			spike.position.x = (i + 1) * 32
+			spike.position.y = 0
+	else:
+		_update_spawn()
+
+
+func _set_width_left(_width_left):
+	width_left = _width_left
+	_update_spawn()
+
+
+func _set_width_right(_width_right):
+	width_right = _width_right
+	_update_spawn()
+
+
+func _clear_childs():
+	var children = get_children()
+	var children_count = get_child_count()
+	for i in range(1, children_count):
+		print(i)
+		var child = children[i]
+		remove_child(child)
+		child.queue_free()
+
+
+func _update_spawn():
+	_clear_childs()
+	var spike = spike_scene.instance()
 	add_child(spike)
 	spike.position = Vector2()
 	for i in range(width_left):
--- Spikes.gd
+++ Spikes.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
 func _ready():
+	if not Engine.editor_hint:
+		connect("body_entered", self, "_on_Spikes_body_entered")
 	pass
 
 func _on_Spikes_body_entered(body):
-	if body.is_in_group("Player"):
-		get_tree().call_group("Player", "player_die")
+		if body.is_in_group("Player"):
+			get_tree().call_group("Player", "player_die")
