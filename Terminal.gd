2a992c064840e25067ef4af9898822f769fc7094
alexandre <thiry.alexandre@gmail.com>
2020-02-07 12:12:22 +0100

    Terminal sprite added and animated

--- Terminal.gd
+++ Terminal.gd
@@ -1,5 +1,12 @@
 extends Area2D
 
+var player
+
+func _ready():
+	var player = get_tree().get_nodes_in_group("Player")
+	player[0].connect("toggle_on", self, "_on_toggle_on")
+	player[0].connect("toggle_off", self, "_on_toggle_off")
+
 func _on_Terminal_body_entered(body):
 	if body.is_in_group("Player"):
 		get_tree().set_group("Player", "is_on_terminal", true)
@@ -8,3 +15,10 @@ func _on_Terminal_body_entered(body):
 func _on_Terminal_body_exited(body):
 	if body.is_in_group("Player"):
 		get_tree().set_group("Player", "is_on_terminal", false)
+
+func _on_toggle_on():
+	$AnimatedSprite.play("active")
+
+func _on_toggle_off():
+	$AnimatedSprite.play("default")
+	pass
